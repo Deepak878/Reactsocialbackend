49c1fed139e1440ca3eebbba2c68f06ee0f91e2d
Deepak878 <54785214+Deepak878@users.noreply.github.com>
2023-01-24 17:46:50 +0545

    auth and user CRUD done

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error) => {
+    if (error) {
+      console.log(error);
+    } else console.log("COnnected to MongoDB");
+  }
+);
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running");
