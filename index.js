5f11eaf4b9f566e4cd9adf7eac8cc7fa684769c7
Deepak878 <54785214+Deepak878@users.noreply.github.com>
2023-03-02 14:49:03 +0545

    Done for get friends

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const multer = require("multer");
 var cors = require("cors");
-
+const path = require("path");
 app.use(cors());
 dotenv.config();
-// app.use("/public", express.static("public"));
+//app.use("/public", express.static("public"));
 
 mongoose.connect(
   process.env.MONGO_URL,
@@ -23,9 +24,31 @@ mongoose.connect(
   }
 );
 
+app.use("/images", express.static(path.join(__dirname, "public/images/")));
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+console.log("upload");
+console.log(upload);
+//yes,a dont use localhost type url
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File Uploaded Successfully.");
+  } catch (error) {
+    console.log("yesma");
+    console.log(error);
+  }
+});
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
